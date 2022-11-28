6b497d88d74c4ff8b9f17e93b3f32374a26dafb8
EduardM555 <minnibaev-ef@yandex.ru>
2022-11-28 23:29:31 +0300

    Init commit

--- game2.py
+++ game2.py
@@ -3,7 +3,8 @@
 import numpy as np
 
 def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
+    """Рандомно угадываем число.
+    
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
